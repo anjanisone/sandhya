c3099aab5daf15255cc5b143b643855fb00b0232
anjanisone <emailing2prasanna@gmail.com>
2026-04-18 01:41:25 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,6 @@
-storage_account_name = "stflcsftp"
-container_name = "prod-outbound"
-access_key = "<YOUR_ACCESS_KEY>"
+base_path = "Files/prod-outbound/"
 
-spark.conf.set(
-    f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net",
-    access_key
-)
-
-base_path = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/"
-
-folders = [f.name.replace("/", "") for f in dbutils.fs.ls(base_path) if f.isDir()]
+folders = [f.name.replace("/", "") for f in mssparkutils.fs.ls(base_path) if f.isDir]
 
 from pyspark.sql.functions import current_timestamp
 
@@ -17,5 +8,5 @@ for folder in folders:
     path = base_path + folder + "/*.json"
     df = spark.read.option("multiline", "true").json(path)
     df = df.withColumn("ingestion_time", current_timestamp())
-    table_name = f"outbound_{folder}"
-    df.write.format("delta").mode("overwrite").saveAsTable(table_name)
+    table_path = f"Tables/outbound_{folder}"
+    df.write.format("delta").mode("append").save(table_path)
